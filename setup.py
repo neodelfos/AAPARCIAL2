18e700ec45e1b7cf4c621072d07730c69a39797c
neodelfos <131727927+neodelfos@users.noreply.github.com>
2023-11-03 11:36:57 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='El objetivo de este proyecto de aprendizaje automático es desarrollar un modelo que permita predecir las opiniones de las personas sobre el aborto utilizando datos demográficos.',
+    author='Carlos Ghio',
+    license='MIT',
+)
